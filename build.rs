f751aee9173a080ec8e5e5f2b159716f042ce1ab
jWindows 1 <jacobsamro@outlook.com>
2026-03-28 01:40:25 -0700

    Use transparent Windows app icon

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 #[cfg(windows)]
 fn main() {
-    println!("cargo:rerun-if-changed=assets/gitspark.jpg");
+    println!("cargo:rerun-if-changed=assets/gitspark.png");
     println!("cargo:rerun-if-changed=assets/gitspark.ico");
     println!("cargo:rerun-if-changed=windows/app.rc");
     let _ = embed_resource::compile("windows/app.rc", embed_resource::NONE);
